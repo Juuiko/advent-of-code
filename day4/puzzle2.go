af4909c47bfbf63bdc7a9177bf9dd88b722ff4af
Juuiko <mahonal@tcd.ie>
2020-03-20 02:31:00 +0000

    Finished day 4

---
+++ day4/puzzle2.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	lowerBound, upperBound := 108457, 562041
+	totalNums := 0
+	for i:=lowerBound; i<=upperBound; i++ {
+		n := strconv.Itoa(i)
+		if (n[0]==n[1]&&n[1]!=n[2])||(n[0]!=n[1]&&n[1]==n[2]&&n[2]!=n[3])||(n[1]!=n[2]&&n[2]==n[3]&&n[3]!=n[4])||(n[2]!=n[3]&&n[3]==n[4]&&n[4]!=n[5])||(n[3]!=n[4]&&n[4]==n[5]) {
+			if n[0]<=n[1]&&n[1]<=n[2]&&n[2]<=n[3]&&n[3]<=n[4]&&n[4]<=n[5]{
+				totalNums++
+			}
+		}
+	}
+	fmt.Println(totalNums)
+}
